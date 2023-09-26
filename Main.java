778cb7a11fbfbf0cc4e106f9318db78b5a2aa4cc
Ittiphon <136064354+Ittiphon17@users.noreply.github.com>
2023-09-26 22:43:25 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {    
+        UserPassDefault userpass = new UserPassDefault();
+        Login form = new Login(userpass.GetValueOfDic());
+        // new Todo();
+
+    }
+}
