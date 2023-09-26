be243048a85ad402f357a5212c2a4fd434c2238c
Ittiphon <136064354+Ittiphon17@users.noreply.github.com>
2023-09-26 22:27:08 +0700

    Create BoomBoomCheck.java

---
+++ BoomBoomCheck.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class BoomBoomCheck implements Backend {
+    
+    // ถ้ามีชื่อ User นั้นแล้วจะใช้ชื่อนั้นไม่ได้
+    public static boolean isCheckNameBoom(String user) {
+        final File filename = new File("DataUserAndPassword.txt");
+        FileReader fr = null;
+        BufferedReader br = null;
+        String line;
+        String originalUser[];
+
+        try {
+            fr = new FileReader(filename);
+            br = new BufferedReader(fr);
+
+            while ((line = br.readLine()) != null) {
+                originalUser = line.split(",");
+                if (originalUser[0].equals(user)) {                  
+                    br.close();
+                    fr.close();
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public Object GetValueOfDic() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'GetValueOfDic'");
+    }
+} // เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง เหงาจัง
